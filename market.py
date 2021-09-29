67985938df3c8b4c7792929c123988529aedfa14
matheusbaars <matbav@gmail.com>
2021-09-29 17:10:38 -0300

    inclusão de templates

--- market.py
+++ market.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World but bigger</h1>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='Phone')
 
 if __name__ == '__main__':    
     app.run(debug=True)
